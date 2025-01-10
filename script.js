7602f0e97affcc1c472afb9fc8225a39884c646c
ali safaa <alisafaaa33@gmail.com>
2025-01-10 15:10:12 +0300

    done

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let currentIndex = 0;
 
 hamburger_icon.addEventListener("click", () => {
      hamburger_icon.style.display = "none";
+     document.body.style.overflow = "hidden";
      if (window.innerWidth <= 839) {
           close_icon.style.display = "block";
           nav_mobile.style.display = "block";
@@ -19,31 +20,32 @@ close_icon.addEventListener("click", () => {
      if (window.innerWidth <= 839) {
           hamburger_icon.style.display = "block";
      }
+     document.body.style.overflow = "auto"; // enable scrolling
      close_icon.style.display = "none";
      nav_mobile.style.display = "none";
 });
 
-// window.addEventListener("scroll", () => {
-//      const header = document.querySelector("[data-header]");
-//      const logo = document.querySelector("[data-logo]");
-//      const button = document.querySelector("[data-button]");
+window.addEventListener("scroll", () => {
+     const header = document.querySelector("[data-header]");
+     const logo = document.querySelector("[data-logo]");
+     const button = document.querySelector("[data-button]");
 
-//      if (window.scrollY >= 600) {
-//           header.style.backgroundColor = "#efeded";
-//           logo.style.color = "#2ecc71";
-//           button.style.backgroundColor = "#f39c12";
-//           button.style.color = "white";
-//           hamburger_icon.style.color = "black";
-//           close_icon.style.color = "black";
-//      } else {
-//           header.style.backgroundColor = "transparent";
-//           logo.style.color = "white";
-//           button.style.backgroundColor = "white";
-//           button.style.color = "black";
-//           hamburger_icon.style.color = "white";
-//           close_icon.style.color = "white";
-//      }
-// });
+     if (window.scrollY >= 200) {
+          header.style.backgroundColor = "#efeded";
+          logo.style.color = "#2ecc71";
+          button.style.backgroundColor = "#f39c12";
+          button.style.color = "white";
+          hamburger_icon.style.color = "black";
+          close_icon.style.color = "black";
+     } else {
+          header.style.backgroundColor = "transparent";
+          logo.style.color = "white";
+          button.style.backgroundColor = "hsl(12, 88%, 59%)";
+          button.style.color = "white";
+          hamburger_icon.style.color = "white";
+          close_icon.style.color = "white";
+     }
+});
 
 function updateCarousel() {
      card_review.forEach((card, index) => {
@@ -71,7 +73,7 @@ function nextSlide() {
 // Automatically change slides
 setInterval(nextSlide, 8000);
 
-// Optional: Add click functionality for dots
+//Add click functionality for dots
 dots.forEach((dot, index) => {
      dot.addEventListener("click", () => {
           currentIndex = index;
